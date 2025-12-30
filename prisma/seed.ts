47b28bc61286b1d0966dd1fd91da6fac0c98ddcf
aditehh <aditya.aryal888@gmail.com>
2025-12-31 00:47:36 +0545

    this

---
+++ prisma/seed.ts
@@ -0,0 +1,35 @@
+import prisma from "@/lib/db"
+
+async function main() {
+    const blogPost = [{
+        // id: 1,
+        slug: "learn-nextjs-basics",
+        title: "learn Next.js Basics",
+        content: `Learn Next.js Basics
+      Next.js is a popular React framework that helps you build fast and SEO-friendly websites.
+
+      ## Why use Next.js?
+      - Built-in routing system
+      - Server-side rendering and static generation
+      - Great developer experience
+
+      `
+    },
+    ]
+
+    for (const post of blogPost) {
+        await prisma.blogPost.create({
+            data: post,
+        })
+    }
+
+}
+
+main()
+    .catch(e => {
+        console.error(e);
+        process.exit(1);
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
